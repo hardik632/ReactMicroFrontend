8c678748407c57323ff105fa03dd4606dcbe7cc6
hardik632 <40192634+hardik632@users.noreply.github.com>
2024-09-25 17:27:15 +0530

    Add files via upload

---
+++ src/DevOpsSyllabus.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const DevOpsSyllabus = () => (
+  <div>
+    <ul>
+      <li>Module 0: Foundational Terminology and </ul>
+  </div>
+);
+
+export default DevOpsSyllabus;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    port: 3002,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new ModuleFederationPlugin({
+      name: 'devops',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './DevOpsSyllabus': './src/DevOpsSyllabus',
+      },
+    }),
+  ],
+};
